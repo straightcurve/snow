c4ab3d35b78b71abf4cdc175b663a3956c766a41
Sweet Acid <antoniu.ilea@gmail.com>
2021-05-02 08:58:09 +0300

    Implemented client code for testing purposes.

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -3,7 +3,32 @@
 
 class Sandbox : public Snow::Application {
 public:
-    Sandbox() = default;
+    Sandbox() {
+        auto tex = Snow::Resources::load_texture(
+                "assets/textures/korone-derp.png",
+                true,
+                "derp"
+        );
+
+        auto e = registry.create();
+        auto &transform = registry.emplace<Snow::TransformComponent>(e);
+        transform.position = glm::vec2(100, 100);
+        transform.rotation = 30;
+        transform.scale = glm::vec2(100, 100);
+
+        auto &sprite = registry.emplace<Snow::SpriteComponent>(e);
+        sprite.texture = tex.id;
+
+
+        auto e2 = registry.create();
+        auto &transform2 = registry.emplace<Snow::TransformComponent>(e2);
+        transform2.position = glm::vec2(300, 100);
+        transform2.rotation = -45;
+        transform2.scale = glm::vec2(200, 200);
+
+        auto &sprite2 = registry.emplace<Snow::SpriteComponent>(e2);
+        sprite2.texture = tex.id;
+    }
 
     void update() override {
 
