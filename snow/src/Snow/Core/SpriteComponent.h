356c47a3a62fcdb2e655eee622efb67c4396fec9
Sweet Acid <antoniu.ilea@gmail.com>
2021-05-02 04:11:52 +0300

    Added sprite and transform components.

---
+++ snow/src/Snow/Core/SpriteComponent.h
@@ -0,0 +1,14 @@
+//
+// Created by Sweet Acid on 01.05.2021.
+//
+
+#pragma once
+
+#include <pch.h>
+
+namespace Snow {
+    struct SpriteComponent {
+        uint32_t texture;
+        glm::vec3 color;
+    };
+}
---
+++ snow/src/Snow/Core/TransformComponent.h
@@ -0,0 +1,13 @@
+//
+// Created by Sweet Acid on 01.05.2021.
+//
+
+#pragma once
+
+#include <pch.h>
+
+namespace Snow {
+    struct TransformComponent {
+        glm::vec3 position;
+    };
+}
