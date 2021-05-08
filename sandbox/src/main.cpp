c74391c8a2f47e208c64ac4962194fd88df385ce
Sweet Acid <antoniu.ilea@gmail.com>
2021-05-08 06:03:42 +0300

    Live window capturing demo.

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -2,55 +2,89 @@
 #include <Snow.h>
 #include "ExampleWindow.h"
 
+#include <X11/Xlib.h>
+#include <X11/X.h>
+#include <X11/extensions/Xcomposite.h>
+#include <memory>
+
 class Sandbox : public Snow::Application {
 public:
+    Snow::RenderTexture rt;
+    Snow::Shader shader;
+
+    _XImage *image;
+    Display *display;
+    Pixmap pixmap;
+    XID xid = 0x1600006;    //  xwininfo
+    int width;
+    int height;
+
+    ~Sandbox() {
+        XFreePixmap(display, pixmap);
+        XCompositeUnredirectWindow(display, xid, CompositeRedirectAutomatic);
+    }
+
     Sandbox() {
-        auto tex = Snow::Resources::load_texture(
-                "assets/textures/korone-derp.png",
-                true,
-                "derp"
-        );
+        Snow::GUI::add_window(new ExampleWindow());
 
-        const int ppu = 100;
-        const int start_x = 200;
-        const int start_y = 360;
-        const int count = 2500;
-        for (size_t i = 0; i < count; i++) {
-            auto e = registry.create();
-            auto &transform = registry.emplace<Snow::TransformComponent>(e);
-            if (i < count * .5)
-                transform.position = glm::vec2(start_x + i * ppu, start_y - i * ppu);
-            else
-                transform.position = glm::vec2(start_x + (count - i) * ppu, start_y + (count - i) * ppu);
-            transform.rotation = 30;
-            transform.scale = glm::vec2(100, 100);
+        display = XOpenDisplay(nullptr);
 
-            auto &sprite = registry.emplace<Snow::SpriteComponent>(e);
-            sprite.texture = tex.id;
+        // Check if Composite extension is enabled
+        int event_base_return;
+        int error_base_return;
+        if (!XCompositeQueryExtension(display, &event_base_return,
+                                     &error_base_return))
+            SNOW_CORE_WARN("Composite not available!\n");
+
+        // Requests the X server to direct the hierarchy starting at window to off-screen storage
+        XCompositeRedirectWindow(display, xid, CompositeRedirectAutomatic);
+
+        // Preventing the backing pixmap from being freed when the window is hidden/destroyed
+        // If you want the window contents to still be available after the window has been destroyed,
+        // or after the window has been resized (but not yet redrawn), you can increment the backing
+        // pixmaps ref count to prevent it from being deallocated.
+        pixmap = XCompositeNameWindowPixmap(display, xid);
+
+        // Get window attributes
+        XWindowAttributes attr;
+        Status s = XGetWindowAttributes(display, xid, &attr);
+        if (s == 0) {
+            SNOW_ERROR("Failed to get window attributes!");
+            return;
         }
 
-        auto e2 = registry.create();
-        auto &transform2 = registry.emplace<Snow::TransformComponent>(e2);
-        transform2.position = glm::vec2(300, 360);
-        transform2.rotation = -45;
-        transform2.scale = glm::vec2(200, 200);
+        width = attr.width;
+        height = attr.height;
 
-        auto &sprite2 = registry.emplace<Snow::SpriteComponent>(e2);
-        sprite2.texture = tex.id;
+        image = XGetImage(display, xid, 0, 0, width, height, 0xffffffff,
+                          ZPixmap);
+        rt.generate(width, height, image->data);
 
-        Snow::GUI::add_window(new ExampleWindow());
+        auto e = registry.create();
+        auto &transform = registry.emplace<Snow::TransformComponent>(e);
+        transform.position = glm::vec2(0, 0);
+        transform.scale = glm::vec2(1280, 720);
+
+        auto &sprite = registry.emplace<Snow::SpriteComponent>(e);
+        sprite.texture = rt.id;
+
+        shader = Snow::Resources::load_shader(
+                "assets/shaders/base/vertex.shader",
+                "assets/shaders/x11/fragment.shader",
+                nullptr,
+                "base"
+        );
     }
 
     void update() override {
-        auto view = registry.view<Snow::TransformComponent>();
-        for (auto e : view) {
-            auto &transform = view.get<Snow::TransformComponent>(e);
-            transform.rotation += 200 * Snow::Time::deltaTime;
-            transform.position.x += 500.f * Snow::Time::deltaTime;
-            transform.position.y += sinf(transform.position.x / 100) * 500.f * Snow::Time::deltaTime;
-            transform.position.x = (lround(transform.position.x) % 1280);
-            transform.position.y = (lround(transform.position.y) % 720);
-        }
+        image = XGetImage(display, xid, 0, 0, width, height, 0xffffffff,
+                          ZPixmap);
+        if (image == nullptr)
+            return;
+
+        shader.use();
+        rt.update(image->data);
+        image->f.destroy_image(image);
     }
 };
 
